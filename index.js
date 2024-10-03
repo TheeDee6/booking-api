7d80a6874d2f2f6f6d0e5cdf7ce5cf6b7ba08614
TheeDee6 <officialtheodora20@gmail.com>
2024-10-03 17:05:29 +0000

    Added colon

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { bookingRouter } from './routes/all-routes.js';
 import mongoose from 'mongoose';
-import 'dotenv/config'
-import cors from 'cors'
+import 'dotenv/config';
+import cors from 'cors';
 
 //connect to database
 await mongoose.connect(process.env.MONGO_URI);
@@ -12,7 +12,7 @@ const app = express();
 
 const port = 3010
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(bookingRouter);
 
