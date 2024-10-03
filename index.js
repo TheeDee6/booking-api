a8a72495ea72438494a0afb4e3e961a820995a1d
TheeDee6 <officialtheodora20@gmail.com>
2024-10-03 16:36:12 +0000

    Added a live url

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { bookingRouter } from './routes/all-routes.js';
 import mongoose from 'mongoose';
 import 'dotenv/config'
+import cors from 'cors'
 
 //connect to database
 await mongoose.connect(process.env.MONGO_URI);
@@ -11,6 +12,7 @@ const app = express();
 
 const port = 3010
 
+app.use(cors())
 app.use(express.json());
 app.use(bookingRouter);
 
